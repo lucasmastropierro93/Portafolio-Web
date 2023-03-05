db88d30965a45c7d81dcb384661eaf5376474996
lucas <lucasmastro93@gmail.com>
2023-03-05 20:29:36 -0300

    agregue a firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaCHjY4Ii0Xl9UoIQ8uTE7W4XaEY1UXu8",
+  authDomain: "portafolio-web-lucasmastro.firebaseapp.com",
+  projectId: "portafolio-web-lucasmastro",
+  storageBucket: "portafolio-web-lucasmastro.appspot.com",
+  messagingSenderId: "414791591238",
+  appId: "1:414791591238:web:d5ca3b6c59fb7bb601b147"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
